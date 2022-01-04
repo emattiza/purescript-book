87e096c56ce91bf848baa6ab344082726dca22cf
Evan Mattiza <emattiza@gmail.com>
2022-01-03 21:44:54 -0600

    added nix-shell and gitpod.yml

---
+++ shell.nix
@@ -0,0 +1,23 @@
+with import <nixpkgs> {};
+
+let
+  easy-ps = import (fetchFromGitHub {
+    owner = "justinwoo";
+    repo = "easy-purescript-nix";
+    rev = "678070816270726e2f428da873fe3f2736201f42";
+    sha256 = "JEabdJ+3cZEYDVnzgMH/YFsaGtIBiCFcgvVO9XRgiY4=";
+  }) {
+    inherit pkgs;
+  };
+in
+  stdenv.mkDerivation {
+    name = "purescript-ulid";
+
+    buildInputs = [
+      easy-ps.purs
+      easy-ps.spago
+      easy-ps.pulp
+      easy-ps.purescript-language-server
+      yarn
+    ];
+  }
